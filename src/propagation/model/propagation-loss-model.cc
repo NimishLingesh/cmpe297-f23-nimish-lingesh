592d77564e25a593aa758529ca8d81d035dd34d6
Nimish Lingesh <111670449+NimishLingesh@users.noreply.github.com>
2023-10-16 19:55:41 -0700

    fixing bu

--- src/propagation/model/propagation-loss-model.cc
+++ src/propagation/model/propagation-loss-model.cc
@@ -470,95 +470,95 @@ TwoRayGroundPropagationLossModel::DoAssignStreams(int64_t stream)
 
 // ------------------------------------------------------------------------- //
 
-NS_OBJECT_ENSURE_REGISTERED(LogDistancePropagationLossModel);
+// NS_OBJECT_ENSURE_REGISTERED(LogDistancePropagationLossModel);
 
-TypeId
-LogDistancePropagationLossModel::GetTypeId()
-{
-    static TypeId tid =
-        TypeId("ns3::LogDistancePropagationLossModel")
-            .SetParent<PropagationLossModel>()
-            .SetGroupName("Propagation")
-            .AddConstructor<LogDistancePropagationLossModel>()
-            .AddAttribute("Exponent",
-                          "The exponent of the Path Loss propagation model",
-                          DoubleValue(3.0),
-                          MakeDoubleAccessor(&LogDistancePropagationLossModel::m_exponent),
-                          MakeDoubleChecker<double>())
-            .AddAttribute("ReferenceDistance",
-                          "The distance at which the reference loss is calculated (m)",
-                          DoubleValue(1.0),
-                          MakeDoubleAccessor(&LogDistancePropagationLossModel::m_referenceDistance),
-                          MakeDoubleChecker<double>())
-            .AddAttribute("ReferenceLoss",
-                          "The reference loss at reference distance (dB). (Default is Friis at 1m "
-                          "with 5.15 GHz)",
-                          DoubleValue(46.6777),
-                          MakeDoubleAccessor(&LogDistancePropagationLossModel::m_referenceLoss),
-                          MakeDoubleChecker<double>());
-    return tid;
-}
+// TypeId
+// LogDistancePropagationLossModel::GetTypeId()
+// {
+//     static TypeId tid =
+//         TypeId("ns3::LogDistancePropagationLossModel")
+//             .SetParent<PropagationLossModel>()
+//             .SetGroupName("Propagation")
+//             .AddConstructor<LogDistancePropagationLossModel>()
+//             .AddAttribute("Exponent",
+//                           "The exponent of the Path Loss propagation model",
+//                           DoubleValue(3.0),
+//                           MakeDoubleAccessor(&LogDistancePropagationLossModel::m_exponent),
+//                           MakeDoubleChecker<double>())
+//             .AddAttribute("ReferenceDistance",
+//                           "The distance at which the reference loss is calculated (m)",
+//                           DoubleValue(1.0),
+//                           MakeDoubleAccessor(&LogDistancePropagationLossModel::m_referenceDistance),
+//                           MakeDoubleChecker<double>())
+//             .AddAttribute("ReferenceLoss",
+//                           "The reference loss at reference distance (dB). (Default is Friis at 1m "
+//                           "with 5.15 GHz)",
+//                           DoubleValue(46.6777),
+//                           MakeDoubleAccessor(&LogDistancePropagationLossModel::m_referenceLoss),
+//                           MakeDoubleChecker<double>());
+//     return tid;
+// }
 
-LogDistancePropagationLossModel::LogDistancePropagationLossModel()
-{
-}
+// LogDistancePropagationLossModel::LogDistancePropagationLossModel()
+// {
+// }
 
-void
-LogDistancePropagationLossModel::SetPathLossExponent(double n)
-{
-    m_exponent = n;
-}
+// void
+// LogDistancePropagationLossModel::SetPathLossExponent(double n)
+// {
+//     m_exponent = n;
+// }
 
-void
-LogDistancePropagationLossModel::SetReference(double referenceDistance, double referenceLoss)
-{
-    m_referenceDistance = referenceDistance;
-    m_referenceLoss = referenceLoss;
-}
+// void
+// LogDistancePropagationLossModel::SetReference(double referenceDistance, double referenceLoss)
+// {
+//     m_referenceDistance = referenceDistance;
+//     m_referenceLoss = referenceLoss;
+// }
 
-double
-LogDistancePropagationLossModel::GetPathLossExponent() const
-{
-    return m_exponent;
-}
+// double
+// LogDistancePropagationLossModel::GetPathLossExponent() const
+// {
+//     return m_exponent;
+// }
 
-double
-LogDistancePropagationLossModel::DoCalcRxPower(double txPowerDbm,
-                                               Ptr<MobilityModel> a,
-                                               Ptr<MobilityModel> b) const
-{
-    double distance = a->GetDistanceFrom(b);
-    if (distance <= m_referenceDistance)
-    {
-        return txPowerDbm - m_referenceLoss;
-    }
-    /**
-     * The formula is:
-     * rx = 10 * log (Pr0(tx)) - n * 10 * log (d/d0)
-     *
-     * Pr0: rx power at reference distance d0 (W)
-     * d0: reference distance: 1.0 (m)
-     * d: distance (m)
-     * tx: tx power (dB)
-     * rx: dB
-     *
-     * Which, in our case is:
-     *
-     * rx = rx0(tx) - 10 * n * log (d/d0)
-     */
-    double pathLossDb = 10 * m_exponent * std::log10(distance / m_referenceDistance);
-    double rxc = -m_referenceLoss - pathLossDb;
-    NS_LOG_DEBUG("distance=" << distance << "m, reference-attenuation=" << -m_referenceLoss
-                             << "dB, "
-                             << "attenuation coefficient=" << rxc << "db");
-    return txPowerDbm + rxc;
-}
+// double
+// LogDistancePropagationLossModel::DoCalcRxPower(double txPowerDbm,
+//                                                Ptr<MobilityModel> a,
+//                                                Ptr<MobilityModel> b) const
+// {
+//     double distance = a->GetDistanceFrom(b);
+//     if (distance <= m_referenceDistance)
+//     {
+//         return txPowerDbm - m_referenceLoss;
+//     }
+//     /**
+//      * The formula is:
+//      * rx = 10 * log (Pr0(tx)) - n * 10 * log (d/d0)
+//      *
+//      * Pr0: rx power at reference distance d0 (W)
+//      * d0: reference distance: 1.0 (m)
+//      * d: distance (m)
+//      * tx: tx power (dB)
+//      * rx: dB
+//      *
+//      * Which, in our case is:
+//      *
+//      * rx = rx0(tx) - 10 * n * log (d/d0)
+//      */
+//     double pathLossDb = 10 * m_exponent * std::log10(distance / m_referenceDistance);
+//     double rxc = -m_referenceLoss - pathLossDb;
+//     NS_LOG_DEBUG("distance=" << distance << "m, reference-attenuation=" << -m_referenceLoss
+//                              << "dB, "
+//                              << "attenuation coefficient=" << rxc << "db");
+//     return txPowerDbm + rxc;
+// }
 
-int64_t
-LogDistancePropagationLossModel::DoAssignStreams(int64_t stream)
-{
-    return 0;
-}
+// int64_t
+// LogDistancePropagationLossModel::DoAssignStreams(int64_t stream)
+// {
+//     return 0;
+// }
 
 // ------------------------------------------------------------------------- //
 
